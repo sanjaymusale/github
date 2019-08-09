05670662457c3e2ecdaf8371f6c70796f2afb024
sanjaymusale <sanjay@codebrahma.com>
2019-08-09 18:18:10 +0530

    deploy testing

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,5 +1,5 @@
-export const CLIENT_ID = "04648591227caad753ff"
-export const SECRET_ID = process.env.REACT_APP_CLIENT_SECRET
+export const CLIENT_ID = process.env.CLIENT_ID
+export const SECRET_ID = process.env.CLIENT_SECRET
 export const LOGIN_AUTHORIZATION = `https://github.com/login/oauth/authorize?scope=user:email,repo,gist&client_id=${CLIENT_ID}`
 export const LOGIN_WITH_CODE = `https://github.com/login/oauth/access_token`
 export const BASE_URL = 'https://api.github.com'
